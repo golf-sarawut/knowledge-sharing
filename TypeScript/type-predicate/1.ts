ae4610d8d5853256200d118326b687dcaf3b15e7
golf-sarawut <positwinyu.sarawut@moneyforward.co.jp>
2024-06-09 15:15:56 +0900

    add quiz

--- TypeScript/type-predicate/1.ts
+++ TypeScript/type-predicate/1.ts
@@ -10,17 +10,22 @@ type ErrorResponse = {
 
 type ApiResponse<T> = SuccessResponse<T> | ErrorResponse;
 
-function isSuccessResponse<T>(response: ApiResponse<T>){
-  return response.status === "success";
-}
-
 function handleResponse<T>(response: ApiResponse<T>) {
-  if (isSuccessResponse(response)) {
+  if (response.status === "success") {
     console.log("Data:", response.data);
   } else {
     console.log("Error:", response.message);
   }
 }
 
-const response: ApiResponse<string> = { status: "success", data: "result" };
-handleResponse(response);
+function isSuccessResponse<T>(response: ApiResponse<T>) {
+  return response.status === "success";
+}
+
+function handleResponse2<T>(response: ApiResponse<T>) {
+  if (isSuccessResponse(response)) {
+    console.log("Data:", response.data);
+  } else {
+    console.log("Error:", response.message);
+  }
+}
